f5a95775c881b60490e8a35b3703c5deb07851fb
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2025-03-19 14:29:48 +0530

    Touch docs

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -2,7 +2,7 @@ import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
-console.log('Docs 4')
+console.log('Docs 5')
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
