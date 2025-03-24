b649a2c3bcf01d1aa14aa978c97ef8363a7ad1f9
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2025-03-24 14:12:58 +0530

    Touch mobile

--- apps/mobile/App.jsx
+++ apps/mobile/App.jsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// console.log('mobile 1')
+
 export default function App() {
   return (
     <View style={styles.container}>
