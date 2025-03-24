f4209465e25648bf87bffb074fe600f7891e6368
Parth Bhanushali <52828668+Parth-Bhanushali@users.noreply.github.com>
2025-03-24 12:42:06 +0530

    Add a new expo project in apps dir

---
+++ apps/mobile/metro.config.js
@@ -0,0 +1,23 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const path = require('path');
+
+// Find the project and workspace directories
+const projectRoot = __dirname;
+
+// This can be replaced with `find-yarn-workspace-root`
+const monorepoRoot = path.resolve(projectRoot, '../..');
+
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
+
+  // Watch all files within the monorepo
+  config.watchFolders = [monorepoRoot];
+
+  // Let Metro know where to resolve packages and in what order
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(monorepoRoot, 'node_modules'),
+  ];
+
+  return config;
+})();
